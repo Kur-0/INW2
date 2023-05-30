2073762005e095cbfb244adff8ceed56b0dd582c
unknown <cmsonvesso@gmail.com>
2023-05-30 17:25:23 -0300

    AC INW2

--- AC1INW2/app.js
+++ AC1INW2/app.js
@@ -1,26 +1,23 @@
-const url = 'http://localhost:3000/produtos'
+const url = 'http://localhost:3000/produtos';
 fetch(url)
- .then((req) => req.json())
- .then((data) => mostraProdutos(data));
+  .then((req) => req.json())
+  .then((data) => mostraProdutos(data));
 
-function mostraProdutos(produtos){
- const htmlProdutos = produtos.map(
- (produto) =>`<div class="col-md-4 ">
- <div class="card">
-   <img src=${produto.imagem} width=100 height=100 alt="Imagem 1">
-   <div class="card-body overflow-y-hidden">
-     <h5 class="card-title">${produto.descricao}</h5>
-     <p class="card-text">Shampoo Dove</p>
-     <h4>Valor : ${produto.valor}</h4>
-     <a href="#" class="btn" style="background-color: #D92B2B;color: white;">Comprar</a>
-   </div>
- </div>
-</div>
-`
- );
- 
- document.getElementById('app').innerHTML = htmlProdutos;
+ function mostraProdutos(produtos) {
+  const htmlProdutos = produtos.map((produto) => `
+    <div class="col-md-4">
+      <div class="card ">
+        <img src="${produto.imagem}" class="card-img-top width=200 height=200" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${produto.descricao}</h5>
+          <p class="card-text">O melhor teclado custo benefício.</p>
+          <p class="card-text"><medium class="text-body-secondary">R$${produto.valor}</medium></p>
+          <a href="#" class="btn" style="background-color: #D92B2B;color: white;">Comprar</a>
+        </div>
+      </div>
+    </div>
+    `);
+
+  document.getElementById('app').innerHTML = htmlProdutos.join('');
 }
-//  <img src=${produto.imagem} width=100 height=100><br>
-//  <h2>${produto.descricao}</h2>
-//  <h4>Valor : ${produto.valor}</h4>
+
